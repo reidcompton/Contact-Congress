bfe10bb33579e828e620973ac0a4a2ad26c36b79
reidcompton <reidcompton@mac.com>
2016-01-31 22:20:45 +0000

    not working

--- src/Congress/QueryProvider/QueryProvider.cs
+++ src/Congress/QueryProvider/QueryProvider.cs
@@ -7,34 +7,26 @@ using System.Threading.Tasks;
 using System.Collections;
 using System.Linq.Expressions;
 using System.Net;
+using System.Reflection;
 
 namespace Congress
 {
-    internal class QueryProvider : IQueryProvider
+    internal class SunlightQueryProvider : QueryProvider
     {
         private string _apiKey;
-        public QueryProvider(string apiKey)
+        public SunlightQueryProvider(string apiKey)
         {
             _apiKey = apiKey;
         }
-        public IQueryable CreateQuery(Expression expression)
+        public override string GetQueryText(Expression expression)
         {
-            throw new NotImplementedException();
+            return this.Translate(expression);
         }
-
-        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        private string Translate(Expression expression)
         {
-            // possibly implement a switch statement here to detect class and swap in return class
-            //return new Amendments()
-            return new SunlightData<TElement>(_apiKey, expression);
+            return new QueryTranslator().Translate(expression);
         }
-
-        public object Execute(Expression expression)
-        {
-            throw new NotImplementedException();
-        }
-
-        public TResult Execute<TResult>(Expression expression)
+        public override object Execute(Expression expression)
         {
             using (WebClient client = new WebClient())
             {
@@ -66,25 +58,8 @@ namespace Congress
                     path = "votes";
                 client.BaseAddress = string.Format("https://congress.api.sunlightfoundation.com/{2}?apikey={0}&{1}", _apiKey, operation, path);
                 response = client.DownloadString(client.BaseAddress);
-                var typedResult = JsonConvert.DeserializeObject<SunlightResponse<TResult>>(response);
-                // TODO handle .Any() and other return typed calls
-                switch (methodCall.Method.Name)
-                {
-                    case "Where":
-                        return typedResult.Results;
-                    case "Single":
-                        return typedResult.Results;
-                    //return typedResult.Results.ToString().Single();
-                    case "Any":
-                        return typedResult.Results;
-                    //return typedResult.Results.ToString().Count() > 0;
-                    default:
-                        return typedResult.Results;
-                }
-                
-                
+                return response;
             }
-
         }
 
         private static void ParseBinary(BinaryExpression b, List<Tuple<MemberExpression, Expression, BinaryExpression>> membersandUnarysAndBinarys, List<BinaryExpression> binarys)
@@ -96,12 +71,12 @@ namespace Congress
             else
             {
                 binarys.Add(b.Left as BinaryExpression);
-                foreach(BinaryExpression binary in binarys)
+                foreach (BinaryExpression binary in binarys)
                 {
                     membersandUnarysAndBinarys.Add(new Tuple<MemberExpression, Expression, BinaryExpression>(binary.Left as MemberExpression, binary.Right as Expression, binary));
                 }
             }
-                
+
         }
 
         public static object GetRightValue(Expression unary, string type)
@@ -188,12 +163,12 @@ namespace Congress
                 }
                 else
                     membersAndUnarysAndBinarys.Add(new Tuple<MemberExpression, Expression, BinaryExpression>(b.Left as MemberExpression, b.Right as Expression, b));
-                
-                foreach(Tuple<MemberExpression, Expression, BinaryExpression> item in membersAndUnarysAndBinarys)
+
+                foreach (Tuple<MemberExpression, Expression, BinaryExpression> item in membersAndUnarysAndBinarys)
                 {
                     string type = item.Item2.Type.Name;
                     var rightValue = GetRightValue(item.Item2, type);
-                    
+
                     //Pull the JsonProperty off of the class and use that for serialization.
                     ExtractJsonProperties(operation, item.Item1);
 
@@ -209,12 +184,49 @@ namespace Congress
                         operation.Append("__lte");
                     if (item.Item3.NodeType == ExpressionType.NotEqual)
                         operation.Append("__not");
-                    
+
                     operation.AppendFormat("={0}{1}", FormatRightValueAsString(rightValue, type), item == membersAndUnarysAndBinarys.Last() ? "" : "&");
                 }
             }
             return operation.ToString();
         }
+    }
+
+    public abstract class QueryProvider : IQueryProvider
+    {
+        private string _apiKey;
+        public IQueryable CreateQuery(Expression expression)
+        {
+            Type elementType = TypeSystem.GetElementType(expression.Type);
+            try
+            {       
+                return (IQueryable)Activator.CreateInstance(typeof(SunlightData<>).MakeGenericType(elementType), new object[] { this, expression });
+            }
+            catch (TargetInvocationException tie)
+            {
+                throw tie.InnerException;
+            }
+        }
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            // maybe intercept and swap filter and return types here?
+            //return new Amendments()
+            return new SunlightData<TElement>(_apiKey, expression);
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            string response = this.Execute(expression).ToString();
+            var typedResult = JsonConvert.DeserializeObject<SunlightResponse<TResult>>(response);
+            // TODO handle .Any() and other return typed calls
+            return typedResult.Results;
+        }
+
+        public abstract string GetQueryText(Expression expression);
+        public abstract object Execute(Expression expression);
+
+        
     }
     internal class InnermostWhereFinder : ExpressionVisitor
     {
@@ -228,7 +240,6 @@ namespace Congress
 
         protected override Expression VisitMethodCall(MethodCallExpression expression)
         {
-            // check for where, select, etc.
             if (expression.Method.Name == "Where")
                 innermostWhereExpression = expression;
 
@@ -275,7 +286,7 @@ namespace Congress
         {
             get
             {
-                return new QueryProvider(_apiKey);
+                return new SunlightQueryProvider(_apiKey);
             }
         }
 
@@ -288,6 +299,177 @@ namespace Congress
         IEnumerator IEnumerable.GetEnumerator()
         {
             return (Provider.Execute<System.Collections.IEnumerable>(Expression)).GetEnumerator();
+        }
+    }
+
+    internal static class TypeSystem
+    {
+        internal static Type GetElementType(Type seqType)
+        {
+            Type ienum = FindIEnumerable(seqType);
+            if (ienum == null) return seqType;
+            return ienum.GetGenericArguments()[0];
+        }
+        private static Type FindIEnumerable(Type seqType)
+        {
+            if (seqType == null || seqType == typeof(string))
+                return null;
+            if (seqType.IsArray)
+                return typeof(IEnumerable<>).MakeGenericType(seqType.GetElementType());
+            if (seqType.IsGenericType)
+            {
+                foreach (Type arg in seqType.GetGenericArguments())
+                {
+                    Type ienum = typeof(IEnumerable<>).MakeGenericType(arg);
+                    if (ienum.IsAssignableFrom(seqType))
+                    {
+                        return ienum;
+                    }
+                }
+            }
+            Type[] ifaces = seqType.GetInterfaces();
+            if (ifaces != null && ifaces.Length > 0)
+            {
+                foreach (Type iface in ifaces)
+                {
+                    Type ienum = FindIEnumerable(iface);
+                    if (ienum != null) return ienum;
+                }
+            }
+            if (seqType.BaseType != null && seqType.BaseType != typeof(object))
+            {
+                return FindIEnumerable(seqType.BaseType);
+            }
+            return null;
+        }
+    }
+
+    internal class QueryTranslator : ExpressionVisitor
+    {
+        StringBuilder sb;
+
+        internal QueryTranslator()
+        {
+        }
+
+        internal string Translate(Expression expression)
+        {
+            this.sb = new StringBuilder();
+            this.Visit(expression);
+            return this.sb.ToString();
+        }
+
+        private static Expression StripQuotes(Expression e)
+        {
+            while (e.NodeType == ExpressionType.Quote)
+            {
+                e = ((UnaryExpression)e).Operand;
+            }
+            return e;
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression m)
+        {
+            if (m.Method.DeclaringType == typeof(Queryable) && m.Method.Name == "Where")
+            {
+                this.Visit(m.Arguments[0]);
+                LambdaExpression lambda = (LambdaExpression)StripQuotes(m.Arguments[1]);
+                this.Visit(lambda.Body);
+                return m;
+            }
+            throw new NotSupportedException(string.Format("The method '{0}' is not supported", m.Method.Name));
+        }
+
+        protected override Expression VisitUnary(UnaryExpression u)
+        {
+            switch (u.NodeType)
+            {
+                case ExpressionType.Not:
+                    sb.Append(" NOT ");
+                    this.Visit(u.Operand);
+                    break;
+                default:
+                    throw new NotSupportedException(string.Format("The unary operator '{0}' is not supported", u.NodeType));
+            }
+            return u;
+        }
+
+        protected override Expression VisitBinary(BinaryExpression b)
+        {
+            sb.Append("(");
+            this.Visit(b.Left);
+            switch (b.NodeType)
+            {
+                case ExpressionType.And:
+                    sb.Append("&");
+                    break;
+                case ExpressionType.Equal:
+                    sb.Append("=");
+                    break;
+                case ExpressionType.NotEqual:
+                    sb.Append("__not=");
+                    break;
+                case ExpressionType.LessThan:
+                    sb.Append("__lt=");
+                    break;
+                case ExpressionType.LessThanOrEqual:
+                    sb.Append("__lte=");
+                    break;
+                case ExpressionType.GreaterThan:
+                    sb.Append("__gt=");
+                    break;
+                case ExpressionType.GreaterThanOrEqual:
+                    sb.Append("__gte=");
+                    break;
+                default:
+                    throw new NotSupportedException(string.Format("The binary operator '{0}' is not supported", b.NodeType));
+            }
+            this.Visit(b.Right);
+            sb.Append(")");
+            return b;
+        }
+
+        protected override Expression VisitConstant(ConstantExpression c)
+        {
+            IQueryable q = c.Value as IQueryable;
+            if (q != null)
+            {
+                // assume constant nodes w/ IQueryables are table references
+                sb.Append(q.ElementType.Name);
+            }
+            else if (c.Value == null)
+            {
+                sb.Append("NULL");
+            }
+            else {
+                switch (Type.GetTypeCode(c.Value.GetType()))
+                {
+                    case TypeCode.Boolean:
+                        sb.Append(((bool)c.Value) ? 1 : 0);
+                        break;
+                    case TypeCode.String:
+                        sb.Append("'");
+                        sb.Append(c.Value);
+                        sb.Append("'");
+                        break;
+                    case TypeCode.Object:
+                        throw new NotSupportedException(string.Format("The constant for '{0}' is not supported", c.Value));
+                    default:
+                        sb.Append(c.Value);
+                        break;
+                }
+            }
+            return c;
+        }
+
+        protected override Expression VisitMember(MemberExpression m)
+        {
+            if (m.Expression != null && m.Expression.NodeType == ExpressionType.Parameter)
+            {
+                sb.Append(m.Member.Name);
+                return m;
+            }
+            throw new NotSupportedException(string.Format("The member '{0}' is not supported", m.Member.Name));
         }
     }
 }
